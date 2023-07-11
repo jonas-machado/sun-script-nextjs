e2b14a47789328b2104335e4e3e26e75e73de32b
Jonas <jonas.m.a.1997@gmail.com>
2023-07-11 15:24:42 -0300

    ...

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL: any =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : "http://177.200.131.54:3001";
+const URL: any = "http://177.200.131.54:3001";
 
 export const socket = io(URL, {
   transports: ["websocket"],
